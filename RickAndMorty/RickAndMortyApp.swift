0df78033248c4d0072d93d2161a3609a4212609d
Pranav Kulkarni <pranav.kulkarni@sap.com>
2024-02-26 07:50:36 +0530

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Kulkarni, Pranav on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
