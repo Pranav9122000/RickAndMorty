c22c96b143f057e12b3f57ef70ccceaa6fe1d8d6
Pranav Kulkarni <pranavthor2000@gmail.com>
2024-02-26 20:23:38 +0530

    Creating Models

---
+++ RickAndMorty/APIClients/RMEndpoint.swift
@@ -0,0 +1,14 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Kulkarni, Pranav on 26/02/24.
+//
+
+import Foundation
+
+@frozen enum RMEndpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorty/APIClients/RMRequest.swift
@@ -0,0 +1,12 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Kulkarni, Pranav on 26/02/24.
+//
+
+import Foundation
+
+final class RMRequest {
+    
+}
